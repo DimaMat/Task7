cbc3143496abe34ef31f4dc072dba08521436466
DimaMat <diman150797@gmail.com>
2021-05-31 17:48:56 +0400

    Initial commit

---
+++ modul2/src/test/java/com/example/surname/SurnameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.surname;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurnameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
